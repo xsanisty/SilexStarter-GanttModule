d6ff9ce4a4364b474a515d17cd2f55b2fb875888
Ikhsan Agustian <ikhsan017@gmail.com>
2016-01-05 11:09:17 +0700

    implement scale editing

--- src/Resources/assets/js/ganttloader.js
+++ src/Resources/assets/js/ganttloader.js
@@ -1,3 +1,99 @@
+function setScale(scale) {
+    switch(scale) {
+        case "hour":
+            gantt.config.scale_unit = "day";
+            gantt.config.step = 1;
+            gantt.config.date_scale = "%d %M";
+            gantt.config.subscales = [
+                {unit:"hour", step:1, date:"%G:00" }
+            ];
+            gantt.config.min_column_width = 40;
+            gantt.config.scale_height = 50;
+            gantt.templates.date_scale = null;
+            break;
+        case "day":
+            gantt.config.scale_unit = "day";
+            gantt.config.step = 1;
+            gantt.config.date_scale = "%d %M";
+            gantt.config.subscales = [];
+            gantt.config.scale_height = 27;
+            gantt.templates.date_scale = null;
+            break;
+        case "week":
+            var weekScaleTemplate = function(date){
+                var dateToStr = gantt.date.date_to_str("%d %M");
+                var endDate = gantt.date.add(gantt.date.add(date, 1, "week"), -1, "day");
+                return dateToStr(date) + " - " + dateToStr(endDate);
+            };
+
+            gantt.config.scale_unit = "week";
+            gantt.config.min_column_width = 40;
+            gantt.config.step = 1;
+            gantt.templates.date_scale = weekScaleTemplate;
+            gantt.config.subscales = [
+                {unit:"day", step:1, date:"%D" }
+            ];
+            gantt.config.scale_height = 50;
+            break;
+        case "month":
+            gantt.config.scale_unit = "month";
+            gantt.config.date_scale = "%F, %Y";
+            gantt.config.scale_height = 50;
+            gantt.config.min_column_width = 40;
+
+            gantt.config.subscales = [
+                {unit:"day", step:1, date:"%j" }
+            ];
+            break;
+        case "quarter":
+            gantt.config.scale_unit = "year";
+            gantt.config.step = 1;
+            gantt.config.date_scale = "%Y";
+            gantt.config.min_column_width = 50;
+
+            gantt.config.scale_height = 90;
+
+            function quarterLabel(date){
+                var month = date.getMonth();
+                var q_num;
+
+                if(month >= 9){
+                    q_num = 4;
+                }else if(month >= 6){
+                    q_num = 3;
+                }else if(month >= 3){
+                    q_num = 2;
+                }else{
+                    q_num = 1;
+                }
+
+                return "Q" + q_num;
+            }
+
+            gantt.config.subscales = [
+                {unit:"quarter", step:1, template:quarterLabel},
+                {unit:"month", step:1, date:"%M" }
+            ];
+            break;
+        case "year":
+            gantt.config.scale_unit = "year";
+            gantt.config.step = 1;
+            gantt.config.date_scale = "%Y";
+            gantt.config.min_column_width = 50;
+
+            gantt.config.scale_height = 70;
+            gantt.templates.date_scale = null;
+
+
+            gantt.config.subscales = [
+                {unit:"month", step:1, date:"%M" }
+            ];
+            break;
+        default:
+            break;
+    }
+}
+
 $('.sidebar-toggle').on('click', function()
 {
     setTimeout(function(){
@@ -45,6 +141,7 @@ $('#btn-edit').click(function(e){
 
     for(var a in chartInfo){
         if(a != 'settings') {
+            console.log(a);
             $('#'+a).val(chartInfo[a]);
         } else {
             var settings = chartInfo[a];
@@ -56,6 +153,8 @@ $('#btn-edit').click(function(e){
                 $('#settings_columns_'+settings.columns[s].name+'_align').val(settings.columns[s].align);
                 $('#settings_columns_'+settings.columns[s].name+'_width').val(settings.columns[s].width);
             }
+
+            $('#settings_scale').val(settings.scale);
         }
     }
     var $items = $list.find('.form-group').sort(function (a, b) {
@@ -68,7 +167,7 @@ $('#btn-edit').click(function(e){
     $list.find('input[type=checkbox]').iCheck({
         checkboxClass: 'icheckbox_square-blue',
         radioClass: 'iradio_square-blue',
-        increaseArea: '10%' // optional
+        increaseArea: '10%'
     });
 
     $('#chart-modal').modal('show');
@@ -91,8 +190,10 @@ $('#btn-chart-save').click(function(e){
             global.chartInfo = resp.content.chart;
             gantt.config.columns = resp.content.chart.settings.columns;
             gantt.config.columns.push({name:"add", label:"", width:44 });
-            gantt.init("gantt-container");
-            gantt.load(global.ganttApi+'/task/');
+            setScale(global.chartInfo.settings.scale);
+
+
+            gantt.render();
             btn.prop('disabled', false).text('Save');
 
             if(resp.content.chart.visibility == 'private'){
@@ -128,16 +229,27 @@ $('#btn-chart-invite').click(function(e){
 $('input[type=checkbox]').iCheck({
     checkboxClass: 'icheckbox_square-blue',
     radioClass: 'iradio_square-blue',
-    increaseArea: '10%' // optional
+    increaseArea: '10%'
 });
 
 /** gantt configuration */
 global.chartInfo.settings.columns.push({name:"add", label:"", width:44 });
 gantt.config.grid_width = 0;
+gantt.config.order_branch = true;
+gantt.config.order_branch_free = true;
 
 for( var conf in global.chartInfo.settings.columns) {
     gantt.config.grid_width += parseFloat(global.chartInfo.settings.columns[conf].width);
+
+    if (global.chartInfo.settings.columns[conf].name == 'progress') {
+        global.chartInfo.settings.columns[conf].template = function(task) {
+            var progress = parseFloat(task.progress)*100;
+            return progress.toFixed(0) + '%';
+        }
+    }
 }
+
+setScale(global.chartInfo.settings.scale);
 
 gantt.config.columns  = global.chartInfo.settings.columns;
 gantt.config.xml_date = "%Y-%m-%d %H:%i:%s";
